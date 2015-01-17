7ecb7a2642e9a3cdd9cd453f0a4c53c128992140
Felix Ekness <felixekness@gmail.com>
2015-01-17 10:53:54 -0600

    Splash banner now properly sticks below nav when screen is scrolled past the nav at all screen sizes.

--- dist/js/custom.js
+++ dist/js/custom.js
@@ -1,16 +1,18 @@
-
-var splash_height = $('.splash').height();
+// Splash banner sticky variables
+var splash_height = $('.splash').outerHeight();
+var nav_height = $('.navbar-header').height();
 var splash_from_top = $('.splash').offset().top;
 var slide = true;
 
 $(window).resize(function() {
-    splash_height = $('.splash').height();
-    splash_from_top = $('.splash').offset().top + $('.splash').scrollTop();
+    splash_height = $('.splash').outerHeight();
+    nav_height = $('.navbar-header').height();
+    splash_from_top = $('.splash').offset().top;
 });
 
 // Splash banner sticky function
 $(window).scroll(function() {
-    if ($(window).scrollTop() >= (splash_from_top - splash_height)) {
+    if ($(window).scrollTop() >= (splash_from_top - nav_height)) {
         $('.splash').addClass('sticky');
         if (slide) {
         	$('.body-text').animate({marginTop: "+=" + splash_height}, 0);
