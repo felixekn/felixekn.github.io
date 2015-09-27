6b65fdd2f33984d0fa86b1349b4047efe30889c2
Felix Ekness <felixekness@gmail.com>
2015-09-27 15:08:16 -0500

    Updated Index page to be column oriented and created a contact page

--- dist/js/script.js
+++ dist/js/script.js
@@ -9,7 +9,7 @@ $(document).ready(function() {
 	});
 	//on-view scroll animation
 	var $about = $('.about-us');
-	var $materials = $('.quality-materials');
+	var $materials = $('.materials');
 	var $window = $(window);
 	var window_top_position = $window.scrollTop();
 	var element_heights = [$about.outerHeight(),
@@ -17,14 +17,11 @@ $(document).ready(function() {
 	function check_if_in_view() {
 		window_top_position = $window.scrollTop();
 		if (window_top_position >= element_heights[0] / 2.0) {
-			$about.find($("p")).addClass('in-view');
+			$about.addClass('in-view');
+			$materials.addClass('in-view');
 		} else if (window_top_position == 0) {
-			$about.find($("p")).removeClass('in-view');
-		}
-		if (window_top_position >= element_heights[0] + element_heights[1]) {
-			$materials.find($("section")).addClass('in-view');
-		} else if (window_top_position <= element_heights[0]) {
-			$materials.find($("section")).removeClass('in-view');
+			$about.removeClass('in-view');
+			$materials.removeClass('in-view');
 		}
 	}
 	$window.on('scroll resize', check_if_in_view);
