b310ac855039e7f3e26caa2a4bf310b921313653
Felix Ekness <felixekness@gmail.com>
2015-10-11 22:21:28 -0500

    Added more photos to upholstery and industrial pages as well as created aa scroll down button for the front page. Smooth scrolling for the scroll to top button and the scroll down button was also added.

--- dist/js/script.js
+++ dist/js/script.js
@@ -3,22 +3,31 @@ $(document).ready(function() {
 	var $menu = $('.menu');
 	var $menuToggle = $('.menu-toggle');
 	var $toTop = $('.back-to-top');
+	var $scrollDown = $('.scroll-down-wrapper');
 	var resized;
 	// switches menu to mobile view if screen width or device width are <= 700px
-	function mobileView(menu, menuToggle, toTop) {
+	function mobileView(menu, menuToggle, toTop, scrollDown) {
 		if (window.matchMedia('(max-device-width: 700px)').matches || window.matchMedia('(max-width: 700px)').matches ) {
 			menu.removeClass('desktop');
 			menu.addClass('mobile')
 			menuToggle.addClass('mobile');
-			toTop.css("display", "block");
+			toTop.css({
+				display: "block",
+				opacity: "1"
+			});
+			scrollDown.css("display", "none");
 		} else {
 			menu.removeClass('mobile');
 			menu.addClass('desktop');
 			menuToggle.removeClass('mobile');
-			toTop.css("display", "none");
+			toTop.css({
+				display: "none",
+				opacity: "0"
+			});
+			scrollDown.css("display", "block");
 		}
 	}
-	mobileView($menu, $menuToggle, $toTop);
+	mobileView($menu, $menuToggle, $toTop, $scrollDown);
 
 	// open and closes nav
 	$menuToggle.click(function(event) {
@@ -59,8 +68,25 @@ $(document).ready(function() {
 	window.onresize = function () {
 		clearTimeout(resized);
 		resized = setTimeout(function() {
-			mobileView($menu, $menuToggle, $toTop);
+			mobileView($menu, $menuToggle, $toTop, $scrollDown);
 			imageCover($img);
 		}, 100);
 	};
+
+	// Code stolen from css-tricks for smooth scrolling to an anchor
+	// https://css-tricks.com/snippets/jquery/smooth-scrolling/
+	$(function() {
+  		$('a[href*=#]:not([href=#])').click(function() {
+    		if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      			var target = $(this.hash);
+      			target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      			if (target.length) {
+        			$('html,body').animate({
+          				scrollTop: target.offset().top
+        			}, 1000);
+        			return false;
+      			}
+    		}
+  		});
+	});
 });
